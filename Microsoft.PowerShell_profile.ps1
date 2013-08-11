2d4bc93bf036603ff1f06b9be0b41b5940416d5d
Ryan Morrin <ryan@inspiredagency.co.uk>
2013-08-11 12:44:33 +0100

    Initial commit

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,8 @@
+﻿# the posh-git repo lives in my dev folder but change devDir as appropriate
+$devDir = 'C:\development'
+
+# Load posh-git example profile
+. $devDir\posh-git\profile.example.ps1
+
+# Load alias files
+. $home\Documents\WindowsPowershell\profile_alias.ps1
---
+++ profile_alias.ps1
@@ -0,0 +1,26 @@
+###########################################################
+# Set up some aliases
+###########################################################
+Set-Alias vsAlias ${Env:ProgramFiles(x86)}'\Microsoft Visual Studio 11.0\Common7\IDE\devenv.exe'
+
+Set-Alias todoAlias ${Env:ProgramFiles(x86)}'\Git\Bin\touch.exe'
+
+# Assumes 64bit installation of sublime text 2
+Set-Alias subl ${Env:ProgramW6432}'\Sublime Text 2\sublime_text.exe'
+
+
+###########################################################
+# Now create some functions to allow us to
+# pass parameters to some of our aliased executables.
+# I'm sure there is a nicer way to do this
+###########################################################
+function vssafe { vsAlias /SafeMode }
+
+function vs { vsAlias $args }
+
+function todo { & todoAlias "c:\users\ryan\desktop\$args" }
+
+###########################################################
+# Ding! Fries are done!
+###########################################################
+">>Alias List Loaded"
