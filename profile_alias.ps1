129c282462ae06a0bd194d3ad0b382205fb03475
Ryan Morrin <ryan@inspiredagency.co.uk>
2014-01-03 10:28:12 +0000

    Added alias for VS13

--- profile_alias.ps1
+++ profile_alias.ps1
@@ -5,6 +5,8 @@ Set-Alias vsAlias ${Env:ProgramFiles(x86)}'\Microsoft Visual Studio 10.0\Common7
 
 Set-Alias vs12Alias ${Env:ProgramFiles(x86)}'\Microsoft Visual Studio 11.0\Common7\IDE\VWDExpress.exe'
 
+Set-Alias vs13Alias ${Env:ProgramFiles(x86)}'\Microsoft Visual Studio 12.0\Common7\IDE\VWDExpress.exe'
+
 Set-Alias todoAlias ${Env:ProgramFiles(x86)}'\Git\Bin\touch.exe'
 
 # Assumes 64bit installation of sublime text 2
@@ -21,6 +23,8 @@ function vssafe { vsAlias /SafeMode }
 function vs { vsAlias $args }
 
 function vs12 { vs12Alias $args }
+
+function vs13 {vs13Alias $args}
 
 function todo { & todoAlias $home'\desktop\'$args }
 
